dc74b26ecfee89bc862075a31c8a2172c01bccfb
gudkovm <marat.gudkov@ubisoft.com>
2024-04-20 13:56:08 +0700

    fix

--- dbs/init_db.py
+++ dbs/init_db.py
@@ -1,30 +1,28 @@
-import sqlite3
+import sys
+sys.path.insert(1,'./')
+from utils.common import *
 
 # Connect to a database (or create one if it doesn't exist)
-conn = sqlite3.connect('dbs/calls.db')
-create_table_query = """
-CREATE TABLE IF NOT EXISTS calls (
-	id INTEGER PRIMARY KEY,
-    date TEXT,
-    address TEXT,
-    expected_launch_time TEXT,
-    expected_launch_time_ts TEXT,
-    s_mm2 BOOL,
-    s_ma2 BOOL,
-    s_fa2 BOOL,
-    s_q INTEGER,
-    s_sni INTEGER,
-    mcap_num INTEGER,
-    liq_num INTEGER,
-    launched BOOL,
-    launch_time TEXT,
-    buy BOOL,
-    buy_time TEXT,
-    buy_price REAL
-)
-"""
-
-cursor = conn.cursor()
-cursor.execute(create_table_query)
-conn.commit()
-conn.close()
+with SQLiteDB('dbs/calls2.db') as conn:
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS calls (
+        id INTEGER PRIMARY KEY,
+        date TEXT,
+        address TEXT,
+        expected_launch_time TEXT,
+        expected_launch_time_ts TEXT,
+        s_mm2 BOOL,
+        s_ma2 BOOL,
+        s_fa2 BOOL,
+        s_q INTEGER,
+        s_sni INTEGER,
+        mcap_num INTEGER,
+        liq_num INTEGER,
+        launched BOOL,
+        launch_time TEXT,
+        buy BOOL,
+        buy_time TEXT,
+        buy_price REAL
+    )
+    """
+    conn.execute(create_table_query)
---
+++ utils/common.py
@@ -0,0 +1,42 @@
+import functools
+import traceback
+import sqlite3
+
+
+class SQLiteDB:
+    def __init__(self, database):
+        self.database = database
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.database)
+        return self.conn
+    
+    def execute(self, query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        self.conn.commit()
+        cursor.close()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+
+
+def retry(max_attempts=10, retry_delay=1):
+    def decorator_retry(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            while attempt < max_attempts:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"Attempt {attempt} failed with error: {e}.\nTraceback: {traceback.format_exc()}")
+                    attempt += 1
+                    if attempt < max_attempts:
+                        t.sleep(retry_delay)
+                    else:
+                        print("Max retries reached!")
+            return None
+        return wrapper
+    return decorator_retry
