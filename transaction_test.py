8ab9310e9d27359205f8bee73c2ad3d3262c4f7b
gudkovm <marat.gudkov@ubisoft.com>
2024-04-24 22:37:27 +0700

    tx fixes

--- transaction_test.py
+++ transaction_test.py
@@ -4,8 +4,10 @@ from utils.blockchain import *
 while True:
     start = dt.datetime.now()
     print('attempt start', start)
-    tx_object = prepare_tx(wallet=wallet, asset_out='8AntP7Hxft8Yjr52UQ3RnBHU4njTkDFDho97gE4vRzkt',amount=1, mode='buy', fee='auto')
-    #tx_object = prepare_tx(wallet=wallet, asset_in='8AntP7Hxft8Yjr52UQ3RnBHU4njTkDFDho97gE4vRzkt', mode='sell', fee='auto')
+    #tx_object = prepare_tx(wallet=wallet, asset_in=USDC_ca, asset_out='8AntP7Hxft8Yjr52UQ3RnBHU4njTkDFDho97gE4vRzkt',
+    #                        amount=1, mode='buy', fee='auto')
+    tx_object = prepare_tx(wallet=wallet, asset_in='8AntP7Hxft8Yjr52UQ3RnBHU4njTkDFDho97gE4vRzkt',
+                           asset_out=SOL_ca, mode='sell', fee='auto')
     tx_object['signed_tx'] = sign_tx(tx_object, wallet)
     result = execute_tx(tx_object)
     if result==True:
