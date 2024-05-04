90d0169af1af1bbe9cb3da313ba65ba655e7f174
gudkovm <marat.gudkov@ubisoft.com>
2024-05-04 17:58:18 +0700

    change

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,5 @@ ls $the_path
 rm /home/sc-sniper/dbs/calls.db
 rm /home/sc-sniper/logs/*.log
 python3 /home/sc-sniper/init_db.py
-chmod u+rw /home/sc-sniper/dbs/calls.db
-chmod u+rw /home/sc-sniper/dbs/jobs.sqlite
+chmod a+rw /home/sc-sniper/dbs/calls.db
+chmod a+rw /home/sc-sniper/dbs/jobs.sqlite
